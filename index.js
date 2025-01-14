473820faf76c0b6cb1af25ef9ddededb58e62412
tanvirGHD <155833719+tanvirGHD@users.noreply.github.com>
2025-01-15 00:18:32 +0600

    payment history done

--- index.js
+++ index.js
@@ -36,6 +36,7 @@ async function run() {
     const menuCollection = client.db("bistroDb").collection("menu")
     const reviewsCollection = client.db("bistroDb").collection("reviews")
     const cartCollection = client.db("bistroDb").collection("carts")
+    const paymentCollection = client.db("bistroDb").collection("payments")
 
 
     //jwt related apis
@@ -214,6 +215,34 @@ async function run() {
         clientSecret: paymentIntent.client_secret
       })
     })
+
+    //Payment History
+    app.get('/payments', verifyToken, async(req, res) => {
+      const query = {email: req.params.email}
+      if(req.params.email !== req.decoded.email){
+        return res.status(403).send({message: 'forbidden access'})
+      }
+      const result = await paymentCollection.find(query).toArray();
+      res.send(result);
+    })
+
+
+
+    //payment related API
+    app.post('/payment', async(req, res) =>{
+      const payment = req.body;
+      const paymentResult = await paymentCollection.insertOne(payment);
+
+      //carefully delete each item from the cart
+      console.log('payment info', payment)
+      const query = {_id: {
+        $in: payment.cartIds.map(id => new ObjectId(id))
+      }}
+      const deleteResult = await cartCollection.deleteMany(query)
+      res.send({paymentResult, deleteResult})
+    });
+
+    
 
 
 
